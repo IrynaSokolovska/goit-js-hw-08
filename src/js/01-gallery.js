be03bf04138da02963e5980e61c6ace7535e182b
Iryna Sokolovska <sokirina777@gmail.com>
2023-06-26 09:43:52 +0300

    second update deploy

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,66 +1,89 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
 
-console.log(galleryItems);
 
-import SimpleLightbox from "simplelightbox";
-console.log(SimpleLightbox);
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
+// import SimpleLightbox from "simplelightbox";
+// console.log(SimpleLightbox);
+// // Додатковий імпорт стилів
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
-const list = document.querySelector(".gallery");
-list.style.listStyle = "none";
-const markup = galleryItems
-  .map(
-    ({ preview, original, description }) => 
-    `<li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`
-)
-    .join('');
-  
-list.insertAdjacentHTML('beforeend', markup)
-list.addEventListener("click", (onClick));
+// const list = document.querySelector(".gallery");
+// list.style.listStyle = "none";
 
-function onClick(event) {
-    
-  event.preventDefault()
-  if (event.target.nodeName !== 'IMG'){
-   return
-  }
-    
-  const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}">
-  `)
-  instance.show()     
-}
-  console.log(galleryItems);
-
-// const galleryEl = document.querySelector(".gallery");
-// galleryEl.style.listStyle = "none";
-
-// const pictureCards = createPictureGalery(galleryItems);
-// galleryEl.insertAdjacentHTML('beforeend', pictureCards);
+// const markup = createPictureGalery(galleryItems);
 
 // function createPictureGalery (item){
 // return galleryItems.map(({preview, original, description}) =>{
 //     return`
 //     <li class="gallery__item">
-//    <a class="gallery__link" href="${original}">
-//       <img class="gallery__image" src="${preview}" alt="${description}" />
-//    </a>
-// </li>
-//     `  
+//     <a class="gallery__link" href="${original}">\
+//     <img class="gallery__image" src="${preview}" alt="${description}" />
+//     </a>
+//     </li>`  
 // }).join("");
-// }
-// var lightbox = new SimpleLightbox('.gallery a', 
-// {captionsData: "alt", captionDelay: "250"})
+// }    
+//   var lightbox = new SimpleLightbox('.gallery a', 
+//     { captionsData: "alt", captionDelay: "250" })
+
+
+
+
+
+// import { galleryItems } from './gallery-items.js';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// const galleryEl = document.querySelector('.gallery');
+// const galleryList = galleryItems
+//   .map(item => {
+//     return `<li class="gallery__item">
+//    <a class="gallery__link" href=${item.original}>
+//       <img class="gallery__image" src=${item.preview} alt=${item.description} />
+//    </a>
+// </li>`;
+//   })
+//   .join('');
+
+// galleryEl.innerHTML = galleryList;
+// galleryEl.style.cssText = `list-style: none`;
+
+// var lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+//   showCounter: false,
+//   close: false,
+// });
+// -------------------------------------------------
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const list = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+       src="${preview}" 
+       alt="${description}"/>
+   </a>
+</li>`
+  })
+  .join("");
+
+list.innerHTML = markup;
+list.style.cssText = `list-style: none`;
+
+var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",    
+    captionDelay: 250
+});
+
+console.log(galleryItems);
+
+  
+
+
+
 
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
-//
+import Player from '@vimeo/player';
+import throttle  from "lodash.throttle"
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate',throttle(onPlay, 1000) )
+     function onPlay (e) {
+        localStorage.setItem("videoplayer-current-time", e.seconds) 
+        
+     }
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+
+ 
+{/* <iframe
+  id="vimeo-player"
+  src="https://player.vimeo.com/video/236203659"
+  width="640"
+  height="360"
+  frameborder="0"
+  allowfullscreen
+  allow="autoplay; encrypted-media"
+   ></iframe >
+      
+      
+<iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>
+
+<script src="https://player.vimeo.com/api/player.js"></script>
+<script>
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+</script>
+
+       */}
